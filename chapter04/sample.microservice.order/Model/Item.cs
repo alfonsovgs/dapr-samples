6e634995eeb72b68e396a4eb3312585817b5853e
Alfonso <vc.alfonso@hotmail.com>
2021-03-29 09:02:20 -0600

    order microservice

---
+++ chapter04/sample.microservice.order/Model/Item.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace sample.microservice.order.Model
+{
+    public class Item
+    {
+        public string SKU { get; set; }
+        public int Quantity { get; set; }
+    }
+}
